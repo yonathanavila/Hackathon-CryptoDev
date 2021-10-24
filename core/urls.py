2f4520ad8bc8de71b657649cbbb0db6c33a9c474
Yonathan Avila <50575930+yonathanavila@users.noreply.github.com>
2021-10-23 22:12:29 -0600

    -Reclamar POAP

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
   path('', clsHome.as_view(), name='home'),
-  path('subir/firma/', clsSubirFirmas.as_view(), name='home'),
+  path('subir/firma/', clsSubirFirmas.as_view(), name='subir'),
   path('firmar/', clsTemplateviewHome.as_view(), name='menu'),
   path('list/', clsListFirmas.as_view(), name="listado-firmas"),
 ]
--- core/views.py
+++ core/views.py
@@ -15,10 +15,19 @@ class clsHome(TemplateView):
   template_name = "core/home.html"
   model = UserWallet
 
+  def post(self, request, *args, **kwargs):
+    print("Holaa")
+    PoapLink_qs = PoapLink.objects.last()
+    for i in PoapLink_qs:
+      print(i)
+    # texto = open()
+    return
+
 
 class clsSubirFirmas(CreateView):
   template_name = "core/subir_links/subir_firmas.html"
   form_class = frmFile
+  success_url = reverse_lazy('subir')
 
 
 class clsListFirmas(ListView):
